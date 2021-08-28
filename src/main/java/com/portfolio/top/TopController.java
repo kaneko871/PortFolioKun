87594b77dfb98b5f50bc392a6b21e6388d896b12
kaneko871 <kaneko871@gmail.com>
2021-08-28 17:57:39 +0900

    P1 Simple Web site, First Commit

---
+++ src/main/java/com/portfolio/top/TopController.java
@@ -0,0 +1,16 @@
+package com.portfolio.top;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TopController {
+
+	@GetMapping("/top")
+	public String getTop() {
+		return "top";
+
+	}
+
+
+}
